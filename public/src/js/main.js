9e79f1b501684c7a856858cb6ac67f814f70780d
unknown <vadvoker@gmail.com>
2018-10-09 20:01:55 +0300

    beuatiful view

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,32 +1,77 @@
+$(document).ready(() => {
 
+//modal window
+    let verstka = '<p class="modal_text"> Verstka, eptet\'</p>';
 
-let departments = $('.departments__item');
-let isOpened = false;
-departments.click(function () {
-    if(isOpened) {
-        return false;
+    function openModal (content) {
+        let modal = '<div class="modal"><div class="modal__close">X</div></div>';
+        $('body').prepend(modal);
+        $('.modal').append(content);
+
+        $('.modal__close').on('click', () => {
+            $('.modal').remove();
+        });
     }
-    isOpened = true;
-    $(this).css({'left':`${$(this).offset().left}px`});
-    $(this).css({'top':`${$(this).offset().top}px`});
-    setTimeout(() => {
-        $(this).addClass('departments__item_active');
-        departments.each((i, item) => {
-            if (item !== this) {
-                $(item).addClass('departments__item_disabled');
-            }
-        })
-    },1)
-});
 
-$(document).on('click', (e) => {
-    console.log(isOpened);
-    console.log($(e.target).closest('.departments__item_active').length);
-    if(isOpened && !$(e.target).closest('.departments__item_active').length) {
-        console.log('opened')
-        // departments.removeClass('departments__item_active departments__item_disabled');
-        // setTimeout(() => {
-        //     isOpened = false;
-        // },500)
-    }
+    $('.login').on('click', () => {
+        openModal(verstka);
+    });
+
+
+//dich
+
+    let departments = $('.departments__item');
+    let closeDepartment = $('.close_department');
+    let isOpened = false;
+    let leftPos, topPos;
+
+    departments.on('click',function () {
+        if(isOpened) {
+            return;
+        }
+        leftPos = $(this).offset().left;
+        topPos = $(this).offset().top;
+        $(this).css({'left':`${leftPos - 20}px`});
+        $(this).css({'top':`${topPos - 20}px`});
+        setTimeout(() => {
+            $(this).addClass('departments__item_active');
+            departments.each((i, item) => {
+                if (item !== this) {
+                    $(item).addClass('departments__item_disabled');
+                }
+            });
+            isOpened = true;
+        },1);
+
+        setTimeout(() => {
+            $(this).find('.close_department').addClass('close_department_active');
+        },600);
+
+    });
+
+    closeDepartment.on('click', function () {
+        let departmentItem = $(this).parent();
+        $(this).removeClass('close_department_active');
+        departmentItem
+            .removeClass('departments__item_active')
+            .css({
+                'position':'absolute',
+                'left':`${leftPos}px !important`,
+                'top':`${topPos}px !important`
+            });
+        setTimeout(() => {
+            departmentItem.css({
+                'position':'static',
+                'left':'auto',
+                'top':'auto'
+            });
+            departments.each((i, item) => {
+                if (item !== this) {
+                    $(item).removeClass('departments__item_disabled');
+                }
+            });
+            isOpened = false;
+        },800);
+    });
+
 });
